102ee056a9886135532b61b00e3fcefdd8c65d13
Muslim <risingkratos@gmail.com>
2016-05-27 16:44:29 +0600

    blabla

--- registration/urls.py
+++ registration/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django import contrib
+
 from . import views
 
 urlpatterns = [
     url(r'^sign_in', views.auth_view, name='auth'),
-    url(r'^sign_up', views.auth_view, name="reg")
+    url(r'^sign_up', views.reg_view, name="reg")
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -13,4 +13,17 @@ def auth_view(request):
             login(request, user)
             return redirect("index")
     return render(request, "main_app/auth.html")
+
+def reg_view(request):
+    if(request.method == "POST"):
+        print("Got in")
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(email, email, password)
+        user.save()
+        user = authenticate(username=email, password=password)
+        login(request, user)
+        return redirect("index")
+    else:
+        return render(request, "main_app/reg.html")
 
