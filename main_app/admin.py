1956836ea3ea19ed48d43eaf9d25d3ef4eb2bc0e
IskanderRaimbayev <iskander.raimbayev@outlook.com>
2016-06-01 06:08:29 +0000

    Added task app & model

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+
 admin.site.register(TestUserProfile)
 admin.site.register(TestUser)
 admin.site.register(TestProfession)
