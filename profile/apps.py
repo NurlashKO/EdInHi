2e4138faefeac8a1553ba35987653f909358473b
Muslim <risingkratos@gmail.com>
2016-06-02 12:55:20 +0600

    Merge remote-tracking branch 'remotes/origin/MuslimBranch'
    # Conflicts:
    #	.idea/workspace.xml
    #	EdInHi/urls.py
    #	db.sqlite3
    #	main_app/models.py
    #	main_app/templates/main_app/base.html
    #	main_app/templates/main_app/reg.html
    #	registration/urls.py
    #	registration/views.py

--- profile/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProfileControllerConfig(AppConfig):
-    name = 'profile'
--- profile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django import contrib
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.profile_view, name='profile'),
-]
