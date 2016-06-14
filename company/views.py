d632340170b832623eee1c0a0a4477a4ca8a7b38
Muslim <risingkratos@gmail.com>
2016-06-14 19:43:15 +0600

    CRUD!

--- company/views.py
+++ company/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.forms import ModelForm
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from company.models import Vacancy
 from profile.forms import UploadFileForm
@@ -25,7 +25,8 @@ def company_view(request):
                 newUser.abstractuser.save()
 
         else:
-            return render(request, 'company/company.html', {'user': request.user.abstractuser, 'vacancies' : request.user.abstractuser.vacancies.all()})
+            return render(request, 'company/company.html',
+                          {'user': request.user.abstractuser, 'vacancies': request.user.abstractuser.vacancies.all()})
 
 
 class VacancyForm(ModelForm):
@@ -33,8 +34,9 @@ class VacancyForm(ModelForm):
         model = Vacancy
         fields = ['name', 'description', 'salary']
 
+
 @login_required
-def company_add_task(request):
+def company_add_vacancy(request):
     form = VacancyForm(request.POST or None)
     if form.is_valid():
         vacancy = form.save(commit=False)
@@ -42,5 +44,22 @@ def company_add_task(request):
         request.user.abstractuser.vacancies.add(vacancy)
         request.user.abstractuser.save()
         print(vacancy.name)
+        return render(request, 'company/company.html',
+                      {'user': request.user.abstractuser, 'vacancies': request.user.abstractuser.vacancies.all()})
+    return render(request, 'company/company_add_vacancy.html', {'form': form})
 
-    return render(request, 'company/company_add_task.html', {'form': form})
+
+@login_required
+def company_delete_vacancy(request, pk):
+    vacancy = get_object_or_404(Vacancy, pk=pk)
+    print(vacancy.name)
+    print("ok")
+    print(request.method)
+    if request.method == 'POST':
+        print("Got in!")
+        request.user.abstractuser.vacancies.remove(vacancy)
+        vacancy.delete()
+        request.user.abstractuser.save()
+        print(vacancy.name)
+        return redirect("company")
+    return render(request, 'company/company_delete_vacancy.html', pk)
