c2f735fb6fed663a635de60254b1876d2848339f
NurlashKO <zh.nurlan96@gmail.com>
2016-06-17 13:48:37 +0600

    fix company profile

--- company/forms.py
+++ company/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Vacancy, CompanyTask
+from main_app.models import AbstractUser
 
 class VacancyForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,8 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = CompanyTask
         fields = ('name', 'comment_to_task', 'description', 'contact_email', 'phone')
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = AbstractUser
+        fields = ('name', 'description', 'web_page', 'email', 'contact_phone', 'logo')
--- company/views.py
+++ company/views.py
@@ -1,27 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.forms import ModelForm, modelformset_factory
+from django.forms import ModelForm
 from django.shortcuts import render, get_object_or_404, redirect
+from profile.forms import UploadFileForm
 
-from company.models import Vacancy, CompanyTask
-from .forms import VacancyForm, TaskForm
+from .models import Vacancy
+from .forms import CompanyForm, VacancyForm, TaskForm
 
 def company_view(request):
+    cform = CompanyForm()
     if request.user.is_authenticated():
         if request.method == "POST":
-            form = UploadFileForm(request.POST, request.FILES)
-            if form.is_valid():
-                new_name = request.POST['name']
-                new_webpage = request.POST['webpage']
-                new_contact_phone = request.POST['contactPhone']
-
-                newUser = request.user
-                newUser.abstractuser.name = new_name
-                newUser.abstractuser.web_page = new_webpage
-                newUser.abstractuser.contact_phone = new_contact_phone
-
-                newUser.abstractuser.profile_image = request.FILES['file']
-                newUser.save()
-                newUser.abstractuser.save()
+            cform = CompanyForm(request.POST, request.FILES, instance=request.user.abstractuser)
+            if cform.is_valid():
+                cform.save()
                 return redirect('/company')
             return redirect('/company')
         else:
@@ -47,17 +38,6 @@ def company_add_vacancy(request):
             request.user.abstractuser.vacancies.add(vacancy)
             request.user.abstractuser.save()
             return redirect('/company')
-        '''
-        vacancy_name = request.POST['vacancy_name']
-        vacancy_comment = request.POST['vacancy_comment']
-        vacancy_description = request.POST['vacancy_description']
-        vacancy_salary = request.POST['vacancy_salary']
-
-        task_name = request.POST['vacancy_name']
-        task_description = request.POST['vacancy_description']
-        task_salary = request.POST['vacancy_salary']
-        task_email
-        '''
     data = {'vform':vform, 'tform':tform}
     return render(request, 'company/company_add_vacancy.html', data)
 
