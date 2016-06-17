f212c9c5d33de5d1190bc0b5cc3a1be36ebba5b9
SanzharAmirzhan <amir.sanzhar@gmail.com>
2016-06-17 14:12:22 +0600

    all vacancies html

--- company/views.py
+++ company/views.py
@@ -45,13 +45,8 @@ def company_add_vacancy(request):
 @login_required
 def company_delete_vacancy(request, pk):
     vacancy = get_object_or_404(Vacancy, pk=pk)
-    print(vacancy.name)
-    print("ok")
-    print(request.method)
     if request.method == 'POST':
-        print("Got in!")
         request.user.abstractuser.vacancies.remove(vacancy)
         vacancy.delete()
         request.user.abstractuser.save()
-        print(vacancy.name)
     return redirect("/company")
