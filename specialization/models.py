336200d9d7b545d5f42ac8d94ca2b8283357120f
Muslim <risingkratos@gmail.com>
2016-06-25 21:36:21 +0600

    Specialization model

--- specialization/models.py
+++ specialization/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from skill.models import Skill
+from tasks.models import Task
 # Create your models here.
+class Specialization(models.Model):
+	skills = models.ForeignKey(Skill)
+	tasks = models.ForeignKey(Task)
+	"""docstring for Specialization"""
