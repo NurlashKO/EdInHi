3946faf1ec299d4c763704a32983c2a3d619cf3c
wormhole23 <kameta96@gmail.com>
2016-06-27 11:36:04 +0600

    LOLOL

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,100 +1,128 @@
-/*
-	Alpha by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+/* ========================================================================= */
+/*  Preloader Script
+/* =========================================================================
 
-(function($) {
+window.onload = function () {
+    document.getElementById('loading-mask').style.display = 'none';
+} */
 
-	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 980px)',
-		narrower: '(max-width: 840px)',
-		mobile: '(max-width: 736px)',
-		mobilep: '(max-width: 480px)'
-	});
+$(function(){
+    /* ========================================================================= */
+    /*  Menu item highlighting
+    /* ========================================================================= */
 
-	$(function() {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$banner = $('#banner');
+    if($("#home").length <= 0){
+        jQuery("#navigation").css("background-color","#00C7FC");
+    }
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+    if($("#home").length > 0){
+        jQuery(window).scroll(function () {
+                if (jQuery(window).scrollTop() > 400) {
+                    jQuery("#navigation").css("background-color","#00C7FC");
+                    jQuery("#navigation").addClass("animated-nav");
+                } else {
+                    jQuery("#navigation").css("background-color","transparent");
+                    jQuery("#navigation").removeClass("animated-nav");
+                }
+        });
 
-		// Prioritize "important" elements on narrower.
-			skel.on('+narrower -narrower', function() {
-				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
-				);
-			});
+        $('#nav').onePageNav({
+            filter: ':not(.external)',
+            scrollSpeed: 950,
+            scrollThreshold: 1
+        });
+    }
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				alignment: 'right'
-			});
+    // Slider Height
+    var slideHeight = $(window).height();
+    $('#home-carousel .carousel-inner .item, #home-carousel .video-container').css('height',slideHeight);
 
-		// Off-Canvas Navigation.
+    $(window).resize(function(){'use strict',
+        $('#home-carousel .carousel-inner .item, #home-carousel .video-container').css('height',slideHeight);
+    });
+    // portfolio filtering
 
-			// Navigation Button.
-				$(
-					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+    $("#projects").mixItUp();
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
+    //fancybox
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navButton, #navPanel, #page-wrapper')
-						.css('transition', 'none');
+    $(".fancybox").fancybox({
+        padding: 0,
 
-		// Header.
-		// If the header is using "alt" styling and #banner is present, use scrollwatch
-		// to revert it back to normal styling once the user scrolls past the banner.
-		// Note: This is disabled on mobile devices.
-			if (!skel.vars.mobile
-			&&	$header.hasClass('alt')
-			&&	$banner.length > 0) {
+        openEffect : 'elastic',
+        openSpeed  : 650,
 
-				$window.on('load', function() {
+        closeEffect : 'elastic',
+        closeSpeed  : 550,
+    });
 
-					$banner.scrollwatch({
-						delay:		0,
-						range:		0.5,
-						anchor:		'top',
-						on:			function() { $header.addClass('alt reveal'); },
-						off:		function() { $header.removeClass('alt'); }
-					});
 
-				});
+    /* ========================================================================= */
+    /*  Facts count
+    /* ========================================================================= */
 
-			}
+    "use strict";
+    $(".fact-item").appear(function () {
+        $(".fact-item [data-to]").each(function () {
+            var e = $(this).attr("data-to");
+            $(this).delay(6e3).countTo({
+                from: 50,
+                to: e,
+                speed: 3e3,
+                refreshInterval: 50
+            })
+        })
+    });
 
-	});
+/* ========================================================================= */
+/*  On scroll fade/bounce fffect
+/* ========================================================================= */
 
-})(jQuery);
+    $("#testimonial").owlCarousel({
+        pagination : true, // Show bullet pagination
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+
+});
+
+/* ========================================================================= */
+/*  On scroll fade/bounce fffect
+/* ========================================================================= */
+
+    wow = new WOW({
+        animateClass: 'animated',
+        offset: 100,
+        mobile: false
+    });
+    wow.init();
+
+/* ---------------------------------------------------------------------- */
+/*      Progress Bars
+/* ---------------------------------------------------------------------- */
+
+initProgress('.progress');
+
+function initProgress(el){
+    jQuery(el).each(function(){
+        var pData = jQuery(this).data('progress');
+        progress(pData,jQuery(this));
+    });
+}
+
+
+            
+function progress(percent, $element) {
+    var progressBarWidth = 0;
+    
+    (function myLoop (i,max) {
+        progressBarWidth = i * $element.width() / 100;
+        setTimeout(function () {   
+        $element.find('div').find('small').html(i+'%');
+        $element.find('div').width(progressBarWidth);
+        if (++i<=max) myLoop(i,max);     
+        }, 10)
+    })(0,percent);  
+}   
