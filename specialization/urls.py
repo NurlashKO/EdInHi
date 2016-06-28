152895e50d7fe1688e27349be3cc21014c3c3789
Muslim <risingkratos@gmail.com>
2016-06-28 16:38:47 +0600

    Specialization template added

--- specialization/urls.py
+++ specialization/urls.py
@@ -4,5 +4,6 @@ from django import contrib
 from . import views
 
 urlpatterns = [
-    url(r'^all_spec$', views.all_specializations, name='all_specializations'),
+    url(r'^all_spec$', views.all_specializations_view, name='all_specializations'),
+    url(r'^spec/(?P<pk>[0-9]+)/$', views.specialization_view, name='specialization'),
 ]
