e0ab0a6efd31b82ef84862354fa26467a3128386
NurlashKO <zh.nurlan96@gmail.com>
2016-07-06 17:22:58 +0600

    merge FrontEndBranch and Master, little changes in admin, skill, media content view

--- media/models.py
+++ media/models.py
@@ -6,8 +6,12 @@ class Book(models.Model):
     description = models.TextField()
     url = models.CharField(max_length=200)
     image = models.CharField(max_length=200, null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class Video(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     url = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
--- skill/models.py
+++ skill/models.py
@@ -11,6 +11,9 @@ class Skill(models.Model):
     specialization = models.ForeignKey(Specialization, blank=True)
     id_in_spec = models.IntegerField(blank=True)
 
+    def __str__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
         self.id_in_spec = self.specialization.skill_set.all().count()+1
         super(Skill, self).save(*args, **kwargs)
