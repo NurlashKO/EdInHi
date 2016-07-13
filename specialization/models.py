b5c7d17138edc67d6617aca752155cb762c606d7
NurlashKO <zh.nurlan96@gmail.com>
2016-07-13 11:23:41 +0600

    specialization

--- specialization/models.py
+++ specialization/models.py
@@ -1,4 +1,5 @@
 from django.db import models
-
+from skill.models import Skill
 class Specialization(models.Model):
 	name = models.CharField(max_length=250, blank=True, default=None)
+	skills = models.ManyToManyField(Skill)
