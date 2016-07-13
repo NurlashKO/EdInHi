e75d482155799e754553988a3e0519dce1eabfb9
NurlashKO <zh.nurlan96@gmail.com>
2016-07-13 13:21:45 +0600

    frontEnd specialization

--- specialization/models.py
+++ specialization/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from skill.models import Skill
 class Specialization(models.Model):
 	name = models.CharField(max_length=250, blank=True, default=None)
+	description = models.TextField(max_length=250, blank=True)
 	skills = models.ManyToManyField(Skill)
