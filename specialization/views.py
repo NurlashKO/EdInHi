734a591c9b831b2b3e3baa0c6aefa5eafba1e1a8
Muslim <risingkratos@gmail.com>
2016-07-14 15:57:58 +0600

    Fixes bug: not logged users specialization

--- specialization/views.py
+++ specialization/views.py
@@ -9,14 +9,18 @@ def all_specializations_view(request):
     all = Specialization.objects.all()
     return render(request, 'specialization/specializations.html', {'all': all})
 
-
 def specialization_view(request, pk):
     specialization = get_object_or_404(Specialization, pk=pk)
     skills = specialization.skills.all()
-    passed_skills = request.user.skill_set.all()
-    passed = 0
-    for skill in skills:
-        if skill in passed_skills:
-            passed+=1
-    progress = int(passed/specialization.skills.all().count() * 100)
+    progress = 0
+    if request.user.is_authenticated():
+        passed_skills = request.user.skill_set.all()
+        passed = 0
+        for skill in skills:
+            if skill in passed_skills:
+                passed+=1
+        progress = int(passed/specialization.skills.all().count() * 100)
+    else:
+            progress = 0
+            passed_skills = None
     return render(request, 'specialization/specialization.html', {'passed_skills':passed_skills, 'specialization': specialization, 'progress': progress})
